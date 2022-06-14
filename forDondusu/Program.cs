ea7e262c6b98af59f362bfff1a1d4ff8adb42481
Öryan Karakaş <63560783+oryan9@users.noreply.github.com>
2022-06-14 17:11:37 +0300

    Add project files.

---
+++ forDondusu/Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+
+
+
+
+
+Console.WriteLine("Başlangıc aralığı:");
+int başlangıc = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Bitiş aralığı:");
+int bitis = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Artış miktarı:");
+int artis = int.Parse(Console.ReadLine());
+
+int sayac = 0;
+int toplam = 0;
+for (int i = başlangıc; i <= bitis; i += artis + 1)
+{
+    sayac++;
+    toplam += i;
+    Console.WriteLine($"{sayac}.sayı: {i}");
+}
+Console.WriteLine($"Toplam:{toplam}");
+
+//------------------------------------------------------------------------
+
+int[] numbers = { 1, 3, 5, 7, 9, 12, 18, 23, 67, 90,120,139,999,2548,156455 };
+int sayac2 = 0;
+int toplam2 = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 3 == 0)
+    {
+        sayac2++;
+        toplam2 += numbers[i];
+        
+        
+        Console.WriteLine($" 3 katları: {numbers[i]} \nToplamlamı:{toplam}");
+        Console.WriteLine("************************************************");
+
+    }
+    else if (numbers[i]%2==1)
+    {
+        
+        Console.WriteLine($" {sayac}.Carpımları: {numbers[i] * numbers[i]}");
+    }
+
+
+}
---
+++ whileDongusu/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+
+//string name = "";
+
+//while (string.IsNullOrEmpty(name))
+//{
+//    Console.WriteLine($"İsminiz:");
+//    name =  Console.ReadLine();
+//    //break;
+//}
+//Console.WriteLine($" Merhaba {name}");
+
+
+string name = "";
+do
+{
+    Console.WriteLine($"isminiz; {name}");
+    name = Console.ReadLine();
+    break;
+} while (string.IsNullOrEmpty(name));
+
+Console.WriteLine($"Heyy: {name} ");
